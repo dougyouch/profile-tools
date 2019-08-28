76a2bd8cd18aa112f92b99134a913d210b96709e
Doug Youch <dougyouch@gmail.com>
2019-08-27 22:55:52 -0400

    added display name, and count_objects_around

--- lib/profile-tools.rb
+++ lib/profile-tools.rb
@@ -8,11 +8,11 @@ class ProfileTools
   end
 
   def profile_instance_method(class_name, method_name)
-    profile_method(Object.const_get(class_name), class_name, method_name)
+    profile_method(Object.const_get(class_name), class_name, method_name, "#{class_name}##{method_name}")
   end
 
   def profile_class_method(class_name, method_name)
-    profile_method(Object.const_get(class_name).singleton_class, class_name, method_name)
+    profile_method(Object.const_get(class_name).singleton_class, class_name, method_name, "#{class_name}.#{method_name}")
   end
 
   def self.load(yaml_file)
@@ -47,19 +47,26 @@ class ProfileTools
     changes
   end
 
+  def self.count_objects_around
+    starting_objects = ObjectSpace.count_objects
+    yield
+    ::ProfileTools.count_objects_changes(starting_objects, ObjectSpace.count_objects)
+  end
+
   private
 
-  def profile_method(kls, class_name, method_name)
+  def profile_method(kls, class_name, method_name, display_name)
     method_name_without_profiling = generate_method_name(method_name.to_s, 'without_profiling')
     method_name_with_profiling = generate_method_name(method_name.to_s, 'with_profiling')
 
     kls.class_eval(
 <<-STR, __FILE__, __LINE__ + 1
 def #{method_name_with_profiling}(*args)
-  ActiveSupport::Notifications.instrument('method.profile_tools', class_name: '#{class_name}', method: '#{method_name}') do |payload|
-    starting_objects = ObjectSpace.count_objects
-    result = #{method_name_without_profiling}(*args)
-    payload[:count_objects] = ::ProfileTools.count_objects_changes(starting_objects, ObjectSpace.count_objects)
+  ActiveSupport::Notifications.instrument('method.profile_tools', class_name: '#{class_name}', method: '#{method_name}', display_name: '#{display_name}') do |payload|
+    result = nil
+    payload[:count_objects] = ::ProfileTools.count_objects_around do
+      result = #{method_name_without_profiling}(*args)
+    end
     result
   end
 end
